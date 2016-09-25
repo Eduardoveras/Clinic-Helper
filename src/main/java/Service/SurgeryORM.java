c67ac394f2be9e806e2dba8cd69a0bf9dad055d2
Djsiclait <djidjellypas92@gmail.com>
2016-09-25 19:04:49 -0400

    Add SurgeryORM class

---
+++ src/main/java/Service/SurgeryORM.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Djidjelly Siclait on 9/25/2016.
+ */
+package Service;
+
+import Entity.Surgery;
+
+public class SurgeryORM extends GenericORMService<Surgery>{
+    private static SurgeryORM instance;
+
+    private SurgeryORM(){
+        super(Surgery.class);
+    }
+
+    public static SurgeryORM getInstance(){
+        if(instance == null)
+            instance = new SurgeryORM();
+        
+        return instance;
+    }
+}
