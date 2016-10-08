fc7fb15448e4a548344d0a67e88c07aecd3e3557
Djsiclait <djidjellypas92@gmail.com>
2016-10-08 19:03:32 -0400

    Create DataQueryService

---
+++ src/main/java/com/clinichelper/Service/DataQueryService.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Djidjelly Siclait on 10/8/2016.
+ */
+package com.clinichelper.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataQueryService {
+    // Repositories
+
+    // Appointment Queries
+}
