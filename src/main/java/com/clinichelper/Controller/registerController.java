9a8711e4154ede467bb0bbbb32ed602ee6f5f387
Eduardo Veras <eduardo.storm@gmail.com>
2016-10-08 11:31:14 -0400

    Total fucking refactor because this project was made by a potatoe

--- src/main/java/com/clinichelper/Controller/registerController.java
+++ src/main/java/com/clinichelper/Controller/registerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class registerController {
+
     @RequestMapping("/solicit")
     public String solicit(){
         return "newpatient";
