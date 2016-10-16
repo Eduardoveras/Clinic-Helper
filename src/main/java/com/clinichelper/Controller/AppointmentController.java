dc421d873b5059d5accb60b23e25e316d8f656ea
Djsiclait <djidjellypas92@gmail.com>
2016-10-15 22:10:57 -0400

    Add appointments Gets and newAppointment Post

--- src/main/java/com/clinichelper/Controller/AppointmentController.java
+++ src/main/java/com/clinichelper/Controller/AppointmentController.java
@@ -4,16 +4,53 @@
 package com.clinichelper.Controller;
 
 import com.clinichelper.Entity.Patient;
+import com.clinichelper.Service.DataEntryAndManagementService;
+import com.clinichelper.Service.DataQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.PersistenceException;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Controller
 public class AppointmentController {
 
+    // Services
+    @Autowired
+    private DataEntryAndManagementService DEAMS;
+    @Autowired
+    private DataQueryService DQS;
+
+    // Gets
+    @GetMapping("/appointment")
+    public ModelAndView fetchAppointmentView(Model model){
+
+        model.addAttribute("appointments", DQS.findAllRegisteredAppointments());
+
+        return new ModelAndView("appointments");
+    }
+
     // Posts
-    // TODO: appointment things
+    @PostMapping("/newAppointment")
+    public String createNweApointment(@RequestParam("date") Date appointmentDate, @RequestParam("time") Timestamp appointmentTime, @RequestParam("jascId") String patientJascId, @RequestParam("description") String appointmentDescription, @RequestParam("access") String appointmentAccessFrom){
+
+        try {
+            DEAMS.createNewAppointment(appointmentDate, appointmentTime, patientJascId, appointmentDescription, appointmentAccessFrom);
+        } catch (PersistenceException exp){
+            //
+        } catch (NullPointerException exp) {
+            //
+        } catch (Exception exp){
+            //
+        }
+        
+        return "redirect:/appointments";
+    }
+
 }
