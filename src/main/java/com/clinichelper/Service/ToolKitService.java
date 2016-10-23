856f64a401ddac67feae895bbf25eb935a41b798
Djsiclait <djidjellypas92@gmail.com>
2016-10-23 14:15:33 -0400

    Add todoList to toolkitService class

--- src/main/java/com/clinichelper/Service/ToolKitService.java
+++ src/main/java/com/clinichelper/Service/ToolKitService.java
@@ -3,9 +3,38 @@
  */
 package com.clinichelper.Service;
 
+import com.clinichelper.Entity.Chore;
+import com.clinichelper.Repository.ChoreRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ToolKitService {
 
+    // Attributes
+    private List<Chore> todoList = new ArrayList<>();
+
+    // Repositories
+    private ChoreRepository choreRepository;
+
+    public void InitializeTodoList(){
+
+        // Adding Custom created tasks
+        FetchCustomTasks();
+    }
+
+    // Auxiliary Functions
+    private void FetchCustomTasks(){
+
+        for (Chore c:
+             choreRepository.findAll()) {
+            todoList.add(c);
+        }
+    }
+
+    private void FetchBirthDateReminders(){
+        
+    }
 }
