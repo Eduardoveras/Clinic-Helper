50596af992395e4e2b70b4053e99ada66cd43e6d
Eduardo Veras <eduardo.storm@gmail.com>
2016-10-23 17:19:23 -0400

    FIXING

--- src/main/java/com/clinichelper/Controller/PatientController.java
+++ src/main/java/com/clinichelper/Controller/PatientController.java
@@ -45,7 +45,20 @@ public class PatientController {
 
     // Posts
     @PostMapping("/newPatient")
-    public String registerNewPatient(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, @RequestParam("idCard") String idCard, @RequestParam("email") String mail, @RequestParam("telephoneNumber") String telephoneNumber, @RequestParam("contactTelephoneNumber") String contactTelephoneNumber, @RequestParam("address") String address, @RequestParam("occupation") String occupation, @RequestParam("dateOfBirth")String dateOfBirth, @RequestParam("gender") String gender, @RequestParam("nationality") String nationality, @RequestParam("countries") String countries, @RequestParam("cities") String cities){
+    public String registerNewPatient(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("idCard") String idCard,
+            @RequestParam("email") String mail,
+            @RequestParam("telephoneNumber") String telephoneNumber,
+            @RequestParam("contactTelephoneNumber") String contactTelephoneNumber,
+            @RequestParam("address") String address,
+            @RequestParam("occupation") String occupation,
+            @RequestParam("dateOfBirth")String dateOfBirth,
+            @RequestParam("gender") String gender,
+            @RequestParam("nationality") String nationality,
+            @RequestParam("countries") String countries,
+            @RequestParam("cities") String cities){
 
         try {
 
@@ -53,14 +66,10 @@ public class PatientController {
 
             DEAMS.createNewPatient(firstName, lastName, idCard, telephoneNumber,
                     contactTelephoneNumber, occupation, gender, mail, new Date(sdf1.parse(dateOfBirth).getTime()), nationality, address, cities, countries);
-        } catch (PersistenceException exp){
-            //
-        } catch(IllegalArgumentException exp){
-            //
-        } catch (NullPointerException exp) {
-            //
+        } catch (PersistenceException | IllegalArgumentException | NullPointerException exp){
+            System.out.println("ERROR EN CREAR PACIENTE");
         } catch (Exception exp){
-            //
+            System.out.println("ERROR EN CREAR PACIENTE");
         }
 
         return "redirect:/patients";
