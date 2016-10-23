a6676f6a72d0e2219d75de6378b0d8ba0c3e993a
Eduardo Veras <eduardo.storm@gmail.com>
2016-10-23 13:32:21 -0400

    Adding Full calendar, making dependencies local, refactoring

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -11,4 +11,21 @@ $(window).load(function() {
             China: ['Hong kong','Namibia']
         }
     );
-})
+});
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+    var calHeight = 1000;
+
+    $('#calendar').fullCalendar({
+        height:calHeight,
+        contentHeight:calHeight,
+        dayClick: function() {
+            alert('a day has been clicked!');
+        }
+    });
+
+
+
+});
