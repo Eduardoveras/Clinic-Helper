d866398c5be32490912c0211aceef6ed94e9e3ba
evasoraya <evasoraya.ca@gmail.com>
2016-10-31 04:37:22 -0400

    create new task modal popup

--- src/main/java/com/clinichelper/Controller/IndexController.java
+++ src/main/java/com/clinichelper/Controller/IndexController.java
@@ -2,12 +2,16 @@ package com.clinichelper.Controller;
 
 import com.clinichelper.Service.DataEntryAndManagementService;
 import com.clinichelper.Service.DataQueryService;
+import com.clinichelper.Tools.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.PersistenceException;
 
 @Controller
 public class IndexController {
@@ -30,6 +34,27 @@ public class IndexController {
     public ModelAndView err(Model model, @RequestParam(value="name", required=false, defaultValue="404") String name) {
         model.addAttribute("name", name);
         return new ModelAndView("layouts/error");
+    }
+
+    @PostMapping("/newTask")
+    public String registerNewPatient(
+            @RequestParam("title") String title,
+            @RequestParam("type") Task type,
+            @RequestParam("description") String description,
+            @RequestParam("clinic") String clinicId
+           )
+            {
+
+        try {
+
+           DEAMS.createNewCustomTask(clinicId, title, type, description);
+        } catch (PersistenceException | IllegalArgumentException | NullPointerException exp){
+            System.out.println("ERROR EN CREAR PACIENTE");
+        } catch (Exception exp){
+            System.out.println("ERROR EN CREAR PACIENTE");
+        }
+
+        return "redirect:/patients";
     }
 
 }
