1ac635f96742d51980f1e27f2e2700dd4beab62b
evasoraya <evasoraya.ca@gmail.com>
2016-11-05 11:40:00 -0400

    just testing

--- src/main/java/com/clinichelper/Controller/PatientController.java
+++ src/main/java/com/clinichelper/Controller/PatientController.java
@@ -41,7 +41,13 @@ public class PatientController {
 
         return new ModelAndView("patients/allPatients");
     }
-    
+    @GetMapping("/patientstest")
+    public ModelAndView fetchAllPatientstestView(Model model){
+        if (!DQS.isUserLoggedIn())
+            return new ModelAndView("redirect:/login");
+
+        return new ModelAndView("patients/test");
+    }
 
 
     @GetMapping("/patient/{id}")
