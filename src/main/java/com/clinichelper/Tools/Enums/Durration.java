75410590724d0c3b6b18a9a5f48dc664010007fd
Djsiclait <djidjellypas92@gmail.com>
2016-11-06 11:07:05 -0400

    Create Durration enum

---
+++ src/main/java/com/clinichelper/Tools/Enums/Durration.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Djidjelly Siclait on 11/6/2016.
+ */
+package com.clinichelper.Tools.Enums;
+
+public enum Durration {
+    HALF_HOUR,
+    HOUR,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX
+}
