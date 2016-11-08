05a82babf2c0058d73fa34ae8eba33e276569baa
Djsiclait <djidjellypas92@gmail.com>
2016-11-07 20:48:38 -0400

    Create PasswordCrypter class

---
+++ src/main/java/com/clinichelper/Tools/Classes/PasswordCrypter.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Djidjelly Siclait on 11/7/2016.
+ */
+package com.clinichelper.Tools.Classes;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+
+public final class PasswordCrypter {
+    private static final int iterations = 20*1000;
+    private static final int saltLen = 32;
+    private static final int desiredKeyLen = 256;
+
+    public static String getSaltedHash(String password) throws Exception{
+        byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+
+        // store the salt with the password
+        return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
+    }
+
+    public static boolean check(String password, String stored) throws Exception{
+        String[] saltAndPass = stored.split("\\$");
+
+        if (saltAndPass.length != 2)
+            throw new IllegalStateException("The stored password have the form 'salt$hash'");
+
+        String hashOfInput = hash(password, Base64.decodeBase64(saltAndPass[0]));
+        return hashOfInput.equals(saltAndPass[1]);
+    }
+
+    private static String hash(String password, byte[] salt) throws Exception{
+        if (password == null || password.length() == 0)
+            throw new IllegalArgumentException("Empty password are not supported.");
+
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKey key = f.generateSecret(new PBEKeySpec(password.toCharArray(), salt, iterations, desiredKeyLen));
+
+        return Base64.encodeBase64String(key.getEncoded());
+    }
+}
